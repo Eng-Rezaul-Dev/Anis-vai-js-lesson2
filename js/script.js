8fa7f17fa294d14236a3010e42734170f4ba888b
Eng-Rezaul-Dev <rezaulflancer@gmail.com>
2024-11-14 04:17:37 +0600

    Invocable function version 1.0.1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 document.write("MY name is Eng MD Rezaul Huq Masud")
 console.log("Rezaul");
+(function(num1,num2){
+    var sum = num1+num2;
+    console.log(sum);
+    document.write("<br/>"+sum);
+})(10,15);
